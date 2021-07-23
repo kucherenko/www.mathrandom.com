0adad7fbf4050ea33bb2016e1f7abc889c18741c
Kostia Palchyk <kosich@users.noreply.github.com>
2021-07-23 21:39:52 +0300

    moved social links to the header

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,72 @@
 <template>
-  <div class="pt-5">
+  <div class="flex pt-5">
     <a href="/">
-      <img width="300" src="~/assets/img/logos/logo_red.svg" />
+      <LogoRed />
     </a>
+
+    <div class="
+      flex
+      flex-column
+      items-center
+      flex-grow
+      justify-center
+      text-2xl
+      font-medium
+      leading-5
+      text-white
+      text-center
+    ">
+      <div class="hidden sm:block">
+        We love technologies.
+      </div>
+    </div>
+
+    <div
+      class="
+        flex flex-wrap
+        justify-center
+        font-medium
+        leading-5
+        text-blue-100
+        whitespace-no-wrap
+        xl:justify-start
+      "
+    >
+      <a
+        href="https://t.me/mathrandomcommunity"
+        class="
+          flex
+          items-center
+          mx-3
+          sm:mx-4
+          xl:ml-0 xl:mr-8
+        "
+      >
+        <img alt="Telegram group" src="@/assets/img/icons/telegram.svg" class="w-12 h-12" />
+      </a>
+      <a
+        href="https://www.youtube.com/c/Mathrandomjavascriptcommunity"
+        class="
+          flex
+          items-center
+          mx-3
+          sm:mx-4
+          xl:ml-0 xl:mr-8
+        "
+      >
+        <img alt="Youtube channel" src="@/assets/img/icons/youtube.svg" class="w-12 h-12" />
+      </a>
+    </div>
+
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import LogoRed from '~/components/Logo/LogoRed.vue'
+
+@Component({
+  components: { LogoRed },
+})
+export default class extends Vue {}
+</script>
