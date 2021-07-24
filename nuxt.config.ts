d334b50e6164fad38579860622b54d2344181837
Kostia Palchyk <kosich@users.noreply.github.com>
2021-07-24 22:47:30 +0300

    dns-prefetch for recaptcha and youtube

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,15 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+
+      // dns-prefetch {{{
+      { rel: "dns-prefetch", href: "https://www.recaptcha.net/" }, // recaptcha
+      { rel: "dns-prefetch", href: "https://www.gstatic.com/" }, // also recaptcha
+      { rel: "dns-prefetch", href: "https://www.youtube.com" } // youtube iframe
+      // }}}
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
