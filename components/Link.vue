41b237e0604bab7ab16e6fba7e62186e0c1f3772
Kostia Palchyk <kosich@users.noreply.github.com>
2021-07-26 15:06:47 +0300

    fixed authors links to have nofollow and open in new tab

--- components/Link.vue
+++ components/Link.vue
@@ -1,10 +1,10 @@
 <template>
-  <a :href="source.href ? source.href : source" class="hover:text-blue-500">
-    <!--    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" :class="'text-'+color" fill="none" viewBox="0 0 24 24"-->
-    <!--         stroke="currentColor">-->
-    <!--      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-    <!--            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />-->
-    <!--    </svg>-->
+  <a
+    :href="source.href ? source.href : source"
+    rel="noopener noreferrer"
+    target="_blank"
+    class="hover:text-blue-500"
+  >
     <font-awesome-icon
       :icon="getIcon(source.href ? source.href : source)"
     ></font-awesome-icon>
