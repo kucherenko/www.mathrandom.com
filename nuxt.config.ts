2981b197add463d10763132c5746997fcc0156b9
Kostia Palchyk <kosich@users.noreply.github.com>
2021-08-01 23:52:41 +0300

    added seo description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "A community of IT professionals: Free webinars, Open public technical interviews, Popular frontend, backend, and mobile projects" }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
