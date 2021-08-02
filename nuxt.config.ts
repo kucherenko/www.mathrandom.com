c0ed1f13df4b27e9beb7dadf760b13cbf6105eb8
apk <kucherenko.andrey@gmail.com>
2021-08-02 13:13:22 +0300

    add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "A community of IT professionals: Free webinars, Open public technical interviews, Popular frontend, backend, and mobile projects" }
+      { hid: "description", name: "description", content: "" }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
@@ -38,9 +38,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/css/base.css'
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
