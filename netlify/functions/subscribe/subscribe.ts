7cfb8f433ba513ecc9c4caf9c0ab7daed219d789
apk <kucherenko.andrey@gmail.com>
2021-11-05 21:01:58 +0200

    feat(subscribe): add logic for subscribe user

--- netlify/functions/subscribe/subscribe.ts
+++ netlify/functions/subscribe/subscribe.ts
@@ -1,12 +1,53 @@
+import axios from 'axios'
 import { Handler } from '@netlify/functions'
 
+const client = require('@sendgrid/client')
+
 export const handler: Handler = async (event) => {
-  const { name = 'stranger' } = event.queryStringParameters as any
-  await console.log(event)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-    }),
+  const body = JSON.parse(event.body ?? '{}')
+  const isRealUser = await checkReCAPTCHA(body.token, 'subscribe')
+  if (isRealUser) {
+    try {
+      const lists = body.listId ? [body.listId] : [process.env.MAIN_LIST_ID]
+      return await addContactToList(
+        [{ email: body.email }],
+        body.subscribeGlobal ? [process.env.MAIN_LIST_ID, ...lists] : lists
+      )
+    } catch (e) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(e),
+      }
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'wrong request' }),
+    }
+  }
+}
+
+async function checkReCAPTCHA(token: string, action: string): Promise<boolean> {
+  const { data }: any = await axios.get(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
+  )
+  return data.success && data.action === action
+}
+
+async function addContactToList(contacts: any[], lists: string[]) {
+  const createListRequest = {
+    method: 'PUT',
+    url: '/v3/marketing/contacts',
+    body: {
+      list_ids: lists,
+      contacts,
+    },
+  }
+  try {
+    const [result] = await client.request(createListRequest)
+    return result.body
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(`Error with subscription ${e.message}`)
   }
 }
