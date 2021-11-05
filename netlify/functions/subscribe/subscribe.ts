8a9ed6367d4fa93f6336cb15a33034ae0c52e6eb
apk <kucherenko.andrey@gmail.com>
2021-11-05 20:12:44 +0200

    fix(functions): fix issue with serverless

--- netlify/functions/subscribe/subscribe.ts
+++ netlify/functions/subscribe/subscribe.ts
@@ -1,8 +1,8 @@
 import { Handler } from '@netlify/functions'
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
-
+  const { name = 'stranger' } = event.queryStringParameters as any
+  await console.log(context)
   return {
     statusCode: 200,
     body: JSON.stringify({
