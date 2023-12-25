922725141a04f30c3055b87b1b9ba04c54f03a2d
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2023-12-25 12:08:57 +0200

    feat(game): add 1000 game to error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -16,7 +16,14 @@
           >,
           <span class="block sm:inline">it should still work.</span>
         </div>
-
+        <div class="m-10">
+          <h1 style="font-size: 100px">
+            <Dice v-for="(slot, index) in slots" :number="slot" :key="index" />
+          </h1>
+        </div>
+        <div class="m-10">
+          <button class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="button" @click="generateSlots">Random</button>
+        </div>
         <div class="text-8xl mt-32">🤞</div>
       </div>
     </Section>
@@ -25,15 +32,35 @@
 
 <script>
 import Section from '~/components/Section.vue'
+import Dice from '~/components/Dice.vue'
 
 export default {
+  data() {
+    return {
+      slots: [],
+    };
+  },
   // you can set a custom layout for the error page
-  components: { Section },
+  components: { Section, Dice },
   layout: 'error',
   props: ['error'],
+
   computed: {
     code() {
       return this.error.statusCode
+    },
+  },
+
+  created() {
+    this.generateSlots()
+  },
+
+  methods: {
+    generateSlots() {
+      this.slots = []
+      for (let i = 0; i < 5; i++) {
+        this.slots.push(Math.floor(Math.random() * 6) + 1)
+      }
     },
   },
 }
